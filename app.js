e7f4397673842b224ce0f2e29a2578b5e710f04a
michellenaim <michellenaim@gmail.com>
2020-10-26 15:17:45 -0700

    Update User Auth Backend

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const User = require("./models/User");
 const users = require("./routes/api/users");
 const passport = require('passport');
 const bodyParser = require("body-parser");
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
-  
+
 app.get("/", (req, res) => res.send("Hello World"));
 
 app.use(passport.initialize());
