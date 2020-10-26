79f5ae011500b88df3ac5e45d2bb47fcafe63d10
michellenaim <michellenaim@gmail.com>
2020-10-26 15:31:55 -0700

    Update User Auth Frontend

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AuthRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './navbar/navbar_container.js';
-import MainPage from './main/main_page.jsx';
+import MainPage from './main_page/main_page.jsx';
 import LoginFormContainer from './session/login_form_container.js';
 import SignupFormContainer from './session/signup_form_container.js';
 
