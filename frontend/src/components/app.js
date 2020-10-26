aba79490835483422df982d077177c4eafea7219
michellenaim <michellenaim@gmail.com>
2020-10-26 15:18:07 -0700

    Add User Auth Frontend

---
+++ frontend/src/components/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AuthRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+import NavBarContainer from './navbar/navbar_container.js';
+import MainPage from './main/main_page.jsx';
+import LoginFormContainer from './session/login_form_container.js';
+import SignupFormContainer from './session/signup_form_container.js';
+
+const App = () => (
+  <div>
+    <NavBarContainer />
+    <Switch>
+      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
---
+++ frontend/src/components/main/main_page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Budgeting App</p>
+        <footer>
+          Copyright &copy; 2020
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
