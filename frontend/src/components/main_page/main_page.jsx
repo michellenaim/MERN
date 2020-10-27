6a04d8048100bf6c1d21fe79a238671e6f1a9e22
michellenaim <michellenaim@gmail.com>
2020-10-26 20:36:36 -0700

    Add styling for navbar

--- frontend/src/components/main_page/main_page.jsx
+++ frontend/src/components/main_page/main_page.jsx
@@ -6,6 +6,7 @@ class MainPage extends React.Component {
     return (
       <div>
         <p>Budgeting App</p>
+        <p>This is the text that explain what our app does.</p>
         <footer>
           Copyright &copy; 2020
         </footer>
--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import '../../stylesheets/nav_bar.scss';
+import logo from "../../stylesheets/images/logo.png";
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -16,17 +18,28 @@ class NavBar extends React.Component {
   getLinks() {
       if (this.props.loggedIn) {
         return (
-            <div>
-                <p>You're logged in!!</p>
-                <button onClick={this.logoutUser}>Logout</button>
-            </div>
+          <div className="nav-bar">
+            <Link to="/">
+              <img className="logo" src={logo} alt="" />
+            </Link>
+            <button className="login-button" onClick={this.logoutUser}>Logout</button>
+          </div>
         );
       } else {
         return (
-            <div>
-                <Link to={'/signup'}>Signup</Link>
-                <Link to={'/login'}>Login</Link>
+          <div className="nav-bar">
+            <Link to="/"><img className="logo" src={logo} alt="" /></Link>
+
+            <div className="nav-right">
+              {this.props.history.location.pathname !== "/login" &&
+                this.props.history.location.pathname !== "/signup" && (
+                  <div>
+                    <Link className="login-button" to={"/signup"}>Signup</Link>
+                    <Link className="login-button" to={"/login"}>Login</Link>
+                  </div>
+                )}
             </div>
+          </div>
         );
       }
   }
@@ -34,7 +47,6 @@ class NavBar extends React.Component {
   render() {
       return (
         <div>
-            <h1>Nav Bar</h1>
             { this.getLinks() }
         </div>
       );
