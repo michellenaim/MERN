9523947948248f2514b0e028698f74084a12dd0f
Michael Castanieto <mcastanieto@gmail.com>
2020-10-29 11:10:44 -0700

    change budget route from auth to protected, add 2 buttons to apply or discard budget split changes

--- frontend/src/components/budget/edit_budget_form_container.js
+++ frontend/src/components/budget/edit_budget_form_container.js
@@ -5,11 +5,15 @@ import EditBudgetForm from './edit_budget_form';
 
 const mapStateToProps = state => {
     return ({
+        // TODO: grab current logged in user's info from state
+        // currentUser: state.entities.users[state.session.currentUser.id]
     });
 };
 
 const mapDispatchToProps = dispatch => {
     return ({
+        // TODO: dispatch updated user ( including their updated budget) to user's reducer
+        // updateUser: (user) => dispatch(updateUser(user))
     });
 };
 
