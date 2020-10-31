a9455289c56f12c6e310f3733bfb1dffc566abfb
michellenaim <michellenaim@gmail.com>
2020-10-31 16:28:26 -0700

    Add sorting to transactions

--- frontend/src/components/transactions/transaction_index.jsx
+++ frontend/src/components/transactions/transaction_index.jsx
@@ -103,6 +103,7 @@ class TransactionIndex extends React.PureComponent{
         // debugger
         let transactionsData
         let noTransactionsInCategory
+        // let sortedData
 
         if (!this.props.transactions.data.transactions.length) {
             transactionsData = (
@@ -111,6 +112,9 @@ class TransactionIndex extends React.PureComponent{
                 </tr>
             )
         } else if (this.props.transactions.data.transactions.map !== undefined){
+            debugger
+            // sortedData = this.props.transactions.data.transactions.sort((a, b) => { return debugger
+            //     (a.date > b.date)})
             transactionsData = this.props.transactions.data.transactions.map(transaction => {
                 return <TransactionIndexItem key={transaction._id} errors={this.props.updateErrors} transaction={transaction} editTransaction={this.props.editTransaction} deleteTransaction={this.props.deleteTransaction} clearUpdatedTransactionErrors={this.props.clearUpdatedTransactionErrors} />
             })
